4579bb9b2ec062aeb1d8b90eca3462826dff77c6
   Bezkhlibnyi Andrii <bezkhlibnyiandrii@BezzPro.local>
2024-09-26 16:38:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//  const FirstName = "Andrii"
+//  FirstName = "Ivan"
+//  console.log("Hello ", FirstName)
+
+let FirstName = "Andrii"
+FirstName = "Andrii Bezkhlibnyi, 35 yers old, from Ukraine"
+
+console. log("Hello ", FirstName)
