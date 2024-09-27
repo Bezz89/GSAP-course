4a7279fb5b975d6c8368b66571d6bf310686fdf1
Bez89 <bezkhlibnyi.89@gmail.com>
2024-09-27 15:04:00 +0300

    first commit

---
+++ Lessons_2.2/app1.js
@@ -0,0 +1,41 @@
+// Number
+// BigInt
+// String
+// Boolean
+// undefined
+// null
+// Symbol
+// Object
+
+const Age = 35
+console. log(typeof Age)
+const BigInt = 222344857348756n
+console. log(typeof BigInt)
+const FirstName = "Name"
+console. log(typeof FirstName)
+
+let age2;
+console. log(age2);
+
+const Obj = {
+    name: "Andrii",
+    age: '28',
+    Inworker: true
+
+}
+
+const num = 10
+const num2 = 5
+console. log (num + num2) ;
+console. log (num - num2) ;
+console. log (num * num2);
+console. log (num / num) ;
+console. log (num % num) ;
+
+
+const titleProject = "Cost estimation";
+const screensValue = "regular, uniqe design, animations";
+const screenPrice = 5000;
+const percentage = 15;
+const fullPrice = 30000;
+const responsive = true;
---
+++ lessons_2.1/app.js
@@ -0,0 +1,11 @@
+//  const FirstName = "Andrii"
+//  const Age = "35 "
+//  const Country = "Ukraine "
+//  console.log("Hello ", FirstName, Age, Country)
+
+let FirstName = "Andrii"
+FirstName = "Andrii Bezkhlibnyi"
+Age = "35 "
+Country = "Ukraine"
+
+console. log("Hello ", FirstName, Age, Country)
