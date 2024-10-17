e2ee0745d753bcdfc30350fca1f52babc7bc46a8
Bez89 <bezkhlibnyi.89@gmail.com>
2024-10-17 16:05:23 +0300

    2.9.Modal_Form

--- 2.9.Modal_Form/2.9.js
+++ 2.9.Modal_Form/2.9.js
@@ -4,26 +4,17 @@ const closeButton = document.querySelector('.close_btn');
 const textBlock2 = document.querySelector('.text-block-2');
 const openModalButton = document.querySelector('.form_btn');
 
+
 document.addEventListener('DOMContentLoaded', function() {
 openModalButton.addEventListener('click', () => {
     modalWrap.style.display = 'flex'; 
 });
 
-
+// Закрытие модального окна при клике на overlay, close_btn или text-block-2
 modalWrap.addEventListener('click', (event) => {
-    // Закрываем, если клик не внутри модального окна
-    if (event.target === modalWrap) {
-        modalWrap.style.display = 'none';
+    // Если клик на оверлее или на элементах закрытия (кнопка закрытия или Text Block 2)
+    if (event.target === modalWrap || event.target.closest('.close_btn') || event.target.closest('.text-block-2')) {
+        modalWrap.style.display = 'none'; // Скрываем модалку
     }
-});
-
-
-closeButton.addEventListener('click', () => {
-    modalWrap.style.display = 'none'; // Скрываем модалку
-});
-
-
-textBlock2.addEventListener('click', () => {
-    modalWrap.style.display = 'none'; // Скрываем модалку
 });
 });
